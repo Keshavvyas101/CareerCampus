923509814d4da9cf7dd73cfcfa502c1be4026732
Keshav <155870552+Keshavvyas101@users.noreply.github.com>
2025-12-18 11:59:14 +0530

    Change API base URL environment variable method

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import {useNavigate} from "react-router-dom";
 import {useAuth} from "../context/AuthContext";
 import {toast} from "react-hot-toast";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = process.env.VITE_API_BASE_URL;
 
 
 const Login = () => {
