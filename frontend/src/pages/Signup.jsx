1e9e46f1572955f1786b6a6c88d6eca465c5f08b
Keshav <155870552+Keshavvyas101@users.noreply.github.com>
2025-12-18 12:00:21 +0530

    Change API base URL import method

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
 import {toast} from "react-hot-toast";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = process.env.VITE_API_BASE_URL;
 
 
 const Signup = () => {
